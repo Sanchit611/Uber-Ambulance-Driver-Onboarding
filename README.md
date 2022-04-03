4bfbef56f6620c1d41e2d2682b9407bbc0303927
Sanchit611 <sanchitnarang611@gmail.com>
2022-04-03 15:21:25 +0530

    first

---
+++ README.md
@@ -0,0 +1,26 @@
+# Shopping-Arena-App
+
+<img src="https://user-images.githubusercontent.com/79566726/148944118-42228985-e29d-4c7a-9bdf-4c0581d0ee14.png"></img>
+
+
+<h2>About this Project: </h2>
+<pre>
+This is a full stack e-commerce website build using NodeJS and MongoDB. It is integrated with user authentication and verification. <br>
+It allows the users to view and choose among various products of their choice, add them to their dynamic shopping cart and leave review and rating if they wish to. <br>
+To the admin it provides many exclusive features like adding new products,deleting the unused ones, changing their prices and descriptions and overall managing the website. <br>
+Check out the details section down below to use the application. Hope you have a good experience. 
+<br>
+
+</pre>
+Project Deployed At : https://shopping-arena-app.herokuapp.com/
+
+<h2>Built With: </h2>
+<pre>HTML | CSS | JAVASCRIPT | EMBEDDED JAVASCRIPT | NODEJS | EXPRESS | MONGODB  </pre>
+
+<h2>Details:</h2>
+<pre>
+Developed by: Sanchit Narang <br>
+E-mail : sanchitnarang611@gmail.com <br>
+<a target="_blank" href="https://shopping-arena-app.herokuapp.com/" >Click Here</a> to view the project. <br>
+</pre>
+
---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const seedDB = require('./seed');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const port = process.env.PORT || 8000;
+
+//const DB='mongodb+srv://spokale165:2023165@swapnil-db.v7cbc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const DB = 'mongodb+srv://Sanchit:sanchitcode@cluster0.x4rvz.mongodb.net/e-commerce?retryWrites=true&w=majority'
+mongoose.connect(DB,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify:false,
+        useCreateIndex:true
+}).then(()=>{
+    console.log('Database connected');
+}).catch((err)=> console.log('No connection'));
+
+// Routes
+const productRoutes = require('./routes/product');
+const authRoutes = require('./routes/auth');
+const cartRoutes = require('./routes/cart');
+
+// mongoose.connect('mongodb://localhost:27017/shopApp',
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify:false,
+//         useCreateIndex:true
+//     })
+//     .then(() => {
+//         console.log("DB Connected");
+//     })
+//     .catch((err) => {
+//         console.log("OH NO ERROR!!!");
+//         console.log(err);
+//     });
+
+
+// seedDB();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
+
+const sessionConfig = {
+    secret: 'weneedsomebettersecret',
+    resave: false,
+    saveUninitialized: true
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+// Initilising the passport and sessions for storing the users info
+app.use(passport.initialize());
+app.use(passport.session());
+
+// configuring the passport to use local strategy
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    res.locals.currentUser = req.user;
+    next();
+})
+
+
+app.get('/', (req, res) => {
+    res.render("home");
+    res.send("LANDING PAGE");
+
+})
+
+app.use(productRoutes);
+app.use(authRoutes);
+app.use(cartRoutes);
+
+
+app.listen(3000, () => {
+    console.log(`Server Started AT PORT 3000`);
+})
